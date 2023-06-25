ee45f9ce568dce5064b034582273a2d58db062c7
andrewcannan <>
2023-06-25 18:15:17 +0000

    add add_task page, add functionality to add a task to task table in db

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker intialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select iniatlization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
